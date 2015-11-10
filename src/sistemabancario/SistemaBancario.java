cfe2f86924356ea6de8676ed7fbfd3d01ab43622
whoami <whoami@localhost.localdomain>
2015-11-10 05:17:29 -0300

    complete project

---
+++ src/sistemabancario/SistemaBancario.java
@@ -0,0 +1,14 @@
+package sistemabancario;
+
+import gui.Login;
+
+public class SistemaBancario {
+
+    public static void main(String[] args) {
+
+        Login l = new Login();
+        l.setVisible(true);
+
+    }
+
+}
